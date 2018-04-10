a295741125c5118d1915e3dbd565f9e343933797
r0n4n <ronan.blanchard35@gmail.com>
2018-04-10 14:30:53 +0200

    adding variables parameters to tune the model and minimize the parameters script

--- power_control_param.m
+++ power_control_param.m
@@ -12,7 +12,7 @@ clear all
 %%%%%%%%%%%%%%% WT_MODEL %%%%%%%%%%
 
 
-mode = 3 ;
+mode = 2 ;
 
 
 switch mode
@@ -35,22 +35,31 @@ switch mode
         Hq = 4e5 ;
     case 2
         
-        Ts = 1e-5 ; % sampling period
+        Ts = 1e-3 ; % sampling period
         fs = 1/Ts ; % sampling period
         
-        L = 10e-3 ; % Inductor of the source
+        L = 10e-2 ; % Inductor of the source
         R = 0.3 ; % Resistance of the source
-        C = 2e-3 ; % DC-link capacitor
+        C = 2e-2 ; % DC-link capacitor
         RL = 80 ; % Load resistance
         fsw = 4e3 ; % Switching frequency
+        Tsw = Ts * 10 ; 
         
         U = 311 ; % Amplitude voltage
         RMS = U / sqrt(2)  ; % Phase voltage V
         f=50 ; % Source voltage frequency
         w=2*pi*f ; % source voltage pulsation
-        Udcref = 500 ; % DC_link voltage (V)
-        Hp = 2000 ;
-        Hq = 2000 ;
+        Udcref = 10 ; % DC_link voltage (V)
+        Hp = 20 ;
+        Hq = 0 ;
+        p = 4 ;  % nombre de paires de pôles 
+         %%%%%%%%%%%% Modélisation et commande d’une génératrice
+            %%% Synchrone à aimants permanents dédiée
+            %%% à la conversion de l’énergie éolienne
+        Rs = 1.137 ; 
+        Ls = 2.7e-3 ; 
+        fluxeff =  0.15 ; 
+        Ka = fluxeff ;  
     case 3
         
         Ts = 1e-5 ; % sampling period
@@ -89,24 +98,33 @@ rpm2radps = pi/30 ; % tr/min ro rad/s
 wm2we = p ; % mecanical velocity to electrical velocity 
 radps2hz = 1/(2*pi) ; % rad/s to Hz
 
+%% Select switch table 
+
+% mode 1  : http://sci-hub.tw/http://ieeexplore.ieee.org/document/673716/ 
+% mode 2 : http://bluebox.ippt.pan.pl/~bulletin/(54-3)287.pdf 
+
+switch_table_mode =1  ; 
 
 
+switch switch_table_mode
+    case 1
+%        
+%         l= load('sp10.mat') ;
+%         spq10=l.sp10 ;
+%         l= load('sp11.mat') ;
+%         spq11=l.sp11;
+%         
+%         l= load('sp00.mat') ;
+%         spq00=l.sp00 ;
+%         
+%         l= load('sp01.mat') ;
+%         spq01=l.sp01 ;
+        [ spq10, spq11, spq00, spq01] = switch_table_init1() ; 
+    case 2
+        [ spq10, spq11, spq00, spq01] = switch_table_init1() ; 
+end
 
-global sp10 sp11 sp00 sp01
 
-
-l= load('sp10.mat') ;
-sp10=l.sp10 ;
-
-l= load('sp11.mat') ;
-sp11=l.sp11;
-
-
-l= load('sp00.mat') ;
-sp00=l.sp00 ;
-
-
-l= load('sp01.mat') ;
-sp01=l.sp01 ;
+%%
 
 
